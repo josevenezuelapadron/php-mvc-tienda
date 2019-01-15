6c6846399297736c3b5f5c937ca734adb3519004
José Padrón <jose.padron712@gmail.com>
2019-01-15 13:57:01 -0400

    Formulario de registro de usuarios

--- controllers/UsuarioController.php
+++ controllers/UsuarioController.php
@@ -2,7 +2,17 @@
 
 class usuarioController {
   public function index() {
-    // echo "Controlador usuarios, accion index";
-    require_once "views/producto/destacados.php";
+    echo "Controlador usuarios, accion index";
+    // require_once "views/producto/destacados.php";
+  }
+
+  public function registro() {
+    require_once "views/usuario/registro.php";
+  }
+
+  public function save() {
+    if (isset($_POST)) {
+      var_dump($_POST);
+    }
   }
 }
