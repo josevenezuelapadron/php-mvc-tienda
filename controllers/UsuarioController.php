bf8936a737be166a29fe30d07c762062bda1ac92
José Padrón <jose.padron712@gmail.com>
2019-01-16 13:09:32 -0400

    Login de usuarios

--- controllers/UsuarioController.php
+++ controllers/UsuarioController.php
@@ -41,4 +41,25 @@ class usuarioController {
     }
     header("Location:".base_url."usuario/registro");
   }
+
+  public function login() {
+    if (isset($_POST)) {
+      $usuario = new Usuario();
+      $usuario->setEmail($_POST['email']);
+      $usuario->setPassword($_POST['password']);
+
+      $identity = $usuario->login();
+      if ($identity && is_object($identity)) {
+        $_SESSION['identity'] = $identity;
+
+        if ($identity->role == "admin") {
+          $_SESSION['admin'] = true;
+        }
+      } else {
+        $_SESSION['error_login'] = "Identificacion fallida";
+      }
+    }
+
+    header("Location:".base_url);
+  }
 }
--- views/layout/sidebar.php
+++ views/layout/sidebar.php
@@ -1,15 +1,19 @@
 <!-- BARRA LATERAL -->
 <aside id="lateral">
   <div id="login" class="block_aside">
-    <form action="#" method="POST">
-      <label for="email">Email:</label>
-      <input type="email" name="email">
+    <?php if (!isset($_SESSION['identity'])): ?>
+      <form action="<?= base_url ?>usuario/login" method="POST">
+        <label for="email">Email:</label>
+        <input type="email" name="email">
 
-      <label for="password">Contraseña:</label>
-      <input type="password" name="password">
+        <label for="password">Contraseña:</label>
+        <input type="password" name="password">
 
-      <input type="submit" value="Enviar">
-    </form>
+        <input type="submit" value="Enviar">
+      </form>
+    <?php else: ?>
+      <h3><? echo $_SESSION['identity']->nombre." ".$_SESSION['identity']->apellidos; ?></h3>
+    <?php endif; ?>
 
     <ul>
       <li><a href="#">Mis pedidos</a></li>
