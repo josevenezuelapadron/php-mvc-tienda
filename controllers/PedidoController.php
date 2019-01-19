253acdcbf037e96b3b9463a6835a61b251386606
José Padrón <jose.padron712@gmail.com>
2019-01-18 20:34:15 -0400

    Carrito de la compra

--- controllers/PedidoController.php
+++ controllers/PedidoController.php
@@ -4,4 +4,8 @@ class pedidoController {
   public function index() {
     echo "Controlador pedidos, accion index";
   }
+
+  public function add() {
+    
+  }
 }
--- views/producto/ver.php
+++ views/producto/ver.php
@@ -13,7 +13,7 @@
     <div class="data">
       <p class="description"><?=$pro->descripcion?></p>
       <p class="price"><?=$pro->precio?> euros</p>
-      <a href="#" class="button">Comprar</a>
+      <a href="<?=base_url?>carrito/add&id=<?=$pro->id?>" class="button">Comprar</a>
     </div>
   </div>
 <?php else: ?>
